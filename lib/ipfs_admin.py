366d0672bfa000173c9260aa883ffccd304621eb
ZeroSum24 <stephen_waddell@hotmail.co.uk>
2019-05-31 12:04:56 +0100

    Installation script (with updated dependencies)

--- lib/ipfs_admin.py
+++ lib/ipfs_admin.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append("lib/")
 from Firmware import *
-import ipfsapi
+import ipfshttpclient
 
 #res = api.get('Qmbds4EKV5wEoXL9bXnxbFbTa9EzYmAkKcTTTYfKWq6PH6')
 #print(res)
@@ -10,33 +10,22 @@ class IPFS_Admin:
 
     def __init__(self,local,verbose = False):
         if local:
-            self.connect(verbose,local,'127.0.0.1')
+            print("Local connection initiated")
+            self.connect(verbose,local,'/ip4/127.0.0.1/tcp/5001')
         else:
+            print("Remote connection initiated")
             self.connect(verbose,local,'https://ipfs.infura.io',)
 
     def connect(self, verbose, local, ip, port = 5001):
-        #TODO: Handle exceptions gracefully
         try:
-            self.api = ipfsapi.connect(ip,port)
-        except:
+            self.api = ipfshttpclient.connect(ip)
+        except Exception as expt:
+            print("Exception:", expt)
             print("Could not connect to IPFS Deamon :(")
-        # except VersionMismatch:
-        #     pass
-        # except ErrorResponse:
-        #     pass
-        # except ConnectionError:
-        #     pass
-        # except StatusError:
-        #     pass
-        # except TimeoutError:
-        #     pass
-        # except:
-        #     pass
 
     def upload_firmware(self,firmware):
         res = self.api.add(firmware.firmware_dir) #pin_add to pin the file as well
         return res
 
     def download_firmware(self,ipfs_link):
-        res = self.api.get(ipfs_link)    
-    
+        res = self.api.get(ipfs_link)
