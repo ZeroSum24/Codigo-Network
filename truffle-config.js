46fa1f9ef85e856ff22d52e23a4d351d58d944f4
BernardChoo <bernardchoowk@gmail.com>
2020-07-15 23:06:05 +0800

    using v4 to compile.

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,30 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+	contracts_build_directory: './client/contracts',
+	networks: {
+		development: {
+			host: 'localhost',
+			port: 8545,
+			network_id: '*' // Match any network id
+		},
+		ropsten: {
+			provider: () => new HDWalletProvider(process.env.ROPSTEN_MNEMONIC, process.env.ROPSTEN_WEB3),
+			network_id: 3, // Ropsten's id
+			gas: 5500000, // Ropsten has a lower block limit than mainnet
+			confirmations: 1, // # of confs to wait between deployments. (default: 0)
+			timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
+			skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
+		}
+	},
+	compilers: {
+		solc: {
+			version: '^0.4.23', // A version or constraint - Ex. "^0.5.0"
+			// Can also be set to "native" to use a native solc
+			parser: 'solcjs' // Leverages solc-js purely for speedy parsing
+		}
+	}
 };
